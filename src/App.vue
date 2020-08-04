a832a3ff3c949550d5b7b85f475dca0cbdf5aab7
sanek29 <sanek-enlogic@yandex.ru>
2020-08-04 21:56:16 +0200

    basic layout

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+</style>
